79e9d91533cecf36c08ec607c2708b23848fbdbf
gergely dancs <dancs.gergelyy@gmail.com>
2025-09-15 11:54:30 +0200

    nem package

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,29 +1,46 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
-import {useTranslation} from "react-i18next";
-import './hero.css'
+import { useTranslation } from "react-i18next";
+import "./hero.css";
 
 const Hero = () => {
-    const {t} = useTranslation();
-    return (
-        <>
-            <section className="hero">
-                <div className="hero-inner">
-                    <div className="hero-text">
-                        <h1>{t('hero.title')}</h1>
-                        <Image className="hero-img" style={{position: "relative"}} src="/images/sprinkler-with-lawn.jpg"
-                               alt={''} width={400} height={400}/>
-                        <p className="hero-motto">{t('hero.motto')}</p>
-                    </div>
-                </div>
-            </section>
-            <div className="post-hero">{t('postHero')}</div>
-            <a href="mailto:info@gartendienst-wien.at" className="cta-button">
-                Jetzt Kontakt aufnehmen
-            </a>
-
-        </>
-    )
-}
+  const { t } = useTranslation();
+  return (
+    <>
+      <section className="hero">
+        <div className="hero-inner">
+          <div className="hero-text">
+            <h1>{t("hero.title")}</h1>
+            <Image
+              className="hero-img"
+              style={{ position: "relative" }}
+              src="/images/sprinkler-with-lawn.jpg"
+              alt={""}
+              width={400}
+              height={400}
+            />
+            <p className="hero-motto">{t("hero.motto")}</p>
+          </div>
+        </div>
+      </section>
+      <div className="post-hero">{t("postHero")}</div>
+      <a
+        style={{ marginRight: "5px" }}
+        href="mailto:info@gartendienst-wien.at"
+        className="cta-button"
+      >
+        Jetzt Kontakt aufnehmen
+      </a>
+      <a
+        href="https://wa.me/436608956377"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="cta-button whatsapp-button"
+      >
+        WhatsApp Nachricht senden
+      </a>
+    </>
+  );
+};
 
 export default Hero;
